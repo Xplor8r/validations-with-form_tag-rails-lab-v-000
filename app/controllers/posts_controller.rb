4e87663c7e87493d6568697a54bf0002ffc0ea59
Michael Pieschel <sixtyhectares@yahoo.com>
2018-10-29 01:45:53 +0000

    automatically backed up by learn

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,7 +2,7 @@ class PostsController < ApplicationController
   def show
     @post = Post.find(params[:id])
   end
-  
+
   def new
     @post = Post.new
   end
@@ -16,7 +16,7 @@ class PostsController < ApplicationController
       render :new
     end
   end
-  
+
   def edit
     @post = Post.find(params[:id])
   end
